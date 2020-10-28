361001e4947616b0262efc93c95423e9e3f51166
Joel <joeldmyers@gmail.com>
2020-10-28 11:13:07 +0100

    basic express server with websockets

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", function connection(ws) {
+  console.log("A new client connected");
+  ws.send("Welcome, New Client!");
+
+  ws.on("message", function incoming(message) {
+    console.log("received %s", message);
+    ws.send(`Got your message, it's: ${message}`);
+  }
+  
+});
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+server.listen(3000, () => console.log("Listening on port 3000"));
