0d8ec39a58ee754100c4216c0c97fb0926ef9fe4
Joel <joeldmyers@gmail.com>
2020-10-28 11:31:05 +0100

    basic group chat functionality set up

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ wss.on("connection", function connection(ws) {
 
   ws.on("message", function incoming(message) {
     console.log("received %s", message);
-    ws.send(`Got your message, it's: ${message}`);
+
+    // Broadcast
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
   });
+
+  
   
 });
 
